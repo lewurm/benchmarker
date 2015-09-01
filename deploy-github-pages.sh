af313bb59b04f4b380a9f7912f30efac31c840e7
Travis CI <bernhard.urban@xamarin.com>
2015-09-01 14:45:30 -0700

    blub

--- deploy-github-pages.sh
+++ deploy-github-pages.sh
@@ -5,12 +5,10 @@ set -e
 # if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
 #     exit 0;
 # fi
-env
-
 git config user.name "Travis CI"
 git config user.email "bernhard.urban@xamarin.com"
 
 git add -f front-end/build
 git commit -am "Deploy to GitHub Pages"
 
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
