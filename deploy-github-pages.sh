30c1543b82d01e4b2cffcbd9ea5aa608d87805fd
Travis CI <bernhard.urban@xamarin.com>
2015-09-01 14:40:05 -0700

    lol

--- deploy-github-pages.sh
+++ deploy-github-pages.sh
@@ -13,4 +13,4 @@ git config user.email "bernhard.urban@xamarin.com"
 git add -f front-end/build
 git commit -am "Deploy to GitHub Pages"
 
-echo git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
